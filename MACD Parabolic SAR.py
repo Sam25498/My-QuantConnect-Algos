b132be4c38630c1b109c679bd8f62a553ae8598e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-03 23:41:41 +0300

    Create MACD Parabolic SAR.py

---
+++ MACD Parabolic SAR.py
@@ -0,0 +1,13 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+
+from datetime import datetime,timedelta
+import numpy as np
+
+Macdlong = None
+EmaLong = None
+EmaShort = None
+PSARLong = None
+PSARShort = None
+
