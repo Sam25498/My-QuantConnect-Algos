a254c46c30b83f6a8fb7605d845d5bac74a1a8bd
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-09 22:45:11 +0300

    Create EMA double cross.py

---
+++ EMA double cross.py
@@ -0,0 +1,10 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+FastisOverSlow = None
+SlowisOverFast = None
+FastisBelowSlow = None
+SlowisBelowFast = None
+
+
