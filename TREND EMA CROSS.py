b0bd9dbfa60503c19846eadac35dfbc25c42b28f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-11 22:13:52 +0300

    Update TREND EMA CROSS.py

--- TREND EMA CROSS.py
+++ TREND EMA CROSS.py
@@ -8,3 +8,13 @@ SlowisOverPrice = None
 PreviousPOverPEMA = None
 PEMAOverPreviousP = None
 
+class CryingRedLemur(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 30)  # Set Start Date
+        self.SetEndDate(2020, 1, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "USDCHF" #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+
