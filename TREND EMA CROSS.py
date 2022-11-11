5395debab2dd7cdfa825877690c2cb89265ff830
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-11 22:13:16 +0300

    Create TREND EMA CROSS.py

---
+++ TREND EMA CROSS.py
@@ -0,0 +1,10 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+
+from datetime import datetime,timedelta
+PriceisOverSlow = None
+SlowisOverPrice = None
+PreviousPOverPEMA = None
+PEMAOverPreviousP = None
+
