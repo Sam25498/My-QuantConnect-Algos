34b3a69b017d8b6b8635e3961b4e30dc218adc12
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-12 23:31:49 +0300

    Create Momentum strategy.py

---
+++ Momentum strategy.py
@@ -0,0 +1,12 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+import numpy as np
+
+Macdlong = None
+AboveSupport = None
+BelowResistance = None
+
+
+  
