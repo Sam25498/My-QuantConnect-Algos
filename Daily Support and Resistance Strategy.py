c0230559f578cae0cdc5b5f70323253a1af8d641
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-13 21:16:35 +0300

    Create Daily Support and Resistance Strategy.py

---
+++ Daily Support and Resistance Strategy.py
@@ -0,0 +1,40 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+import numpy as np
+
+Macdlong = None
+AboveSupport = None
+BelowResistance = None
+
+
+class SwimmingFluorescentPinkShark(QCAlgorithm):
+
+
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)  # Set Start Date
+        self.SetEndDate(2020, 12, 30)
+        self.SetCash(10000)  # Set Strategy Cash
+        
+        self.ticker = "USDCAD"
+        # Rolling Windows to hold bar close data keyed by symbol.
+        self.Data = {}
+
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker, Resolution.Daily, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+        self.tolerance = 0.0025
+        self.toleranceR = 0.986761994
+        self.toleranceS = 1.004000555
+        self.stopLossLevel = -0.05 # stop loss percentage 
+        self.stopProfitLevel = 0.01# stop profit percentage
+            
+        self.SetWarmUp(50, Resolution.Daily)
+        
+        
+    def OnData(self, data):
+        
+        #if self.IsWarmingUp: #Data to warm up the algo is being collected.
+           # return
