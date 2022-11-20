33c625ef2769a874272d0745fc8eb345f4940b34
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-20 23:51:32 +0300

    Update DANCING YELLOW GREEN ALPACE.py

--- DANCING YELLOW GREEN ALPACE.py
+++ DANCING YELLOW GREEN ALPACE.py
@@ -6,3 +6,12 @@ import numpy as np
 class DancingYellowGreenAlpaca(QCAlgorithm):
 
 
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 30)  # Set Start Date
+        self.SetCash(100000)  # Set Strategy Cash
+     
+        tickers = ["EURUSD","USDCAD"]
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.closingData = {}
+        self.SMA45 = {}
+        for ticker in tickers:
