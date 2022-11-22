d36f45acbb29984b49aa3a4b1df5b2e4278328e0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-22 22:10:49 +0300

    Update MACD.py

--- MACD.py
+++ MACD.py
@@ -3,3 +3,5 @@ from AlgorithmImports import *
 #endregion
 # Trade twice a year
 # ----------------------------------------
+ASSETS = ['SPY', 'TLT']; MONTHES = [1, 7];
+# ----------------------------------------
