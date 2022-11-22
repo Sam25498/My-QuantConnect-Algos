2b9437abadb933cafde1a65b4b725b4edffdf73e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-22 22:12:30 +0300

    Update MACD.py

--- MACD.py
+++ MACD.py
@@ -10,3 +10,4 @@ class PermanentPortfolio(QCAlgorithm):
     def Initialize(self):
         self.SetStartDate(2008, 1, 1)               
         self.SetEndDate(2021, 4, 10)
+        self.SetCash(100000)  
