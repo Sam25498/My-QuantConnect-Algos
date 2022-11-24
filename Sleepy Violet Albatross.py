1c5053b4f09f7543450cc1b8afbe68cd8be51165
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-24 23:12:35 +0300

    Update Sleepy Violet Albatross.py

--- Sleepy Violet Albatross.py
+++ Sleepy Violet Albatross.py
@@ -3,3 +3,12 @@ from AlgorithmImports import *
 #endregion
 from datetime import timedelta, datetime
 class FocusedYellowLemur(QCAlgorithm):
+
+
+    def Initialize(self):
+        self.SetStartDate(2015, 12, 1)  # Set Start Date
+        self.SetEndDate(2016, 12, 1)
+        self.SetCash(100000)  # Set Strategy Cash
+       
+        self.Data = {}
+
