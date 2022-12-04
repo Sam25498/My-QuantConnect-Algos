a4800bc2a2a5c643554162083fa575d4f2ea6977
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-04 22:05:55 +0300

    Update Squaregreenbear.py

--- Squaregreenbear.py
+++ Squaregreenbear.py
@@ -2,3 +2,10 @@
 from AlgorithmImports import *
 #endregion
 class SquareGreenBear(QCAlgorithm):
+
+
+    def Initialize(self):
+        self.SetStartDate(2015, 10, 15)  # Set Start Date
+        self.SetEndDate(2020, 10, 15)
+        self.SetCash(100000)  # Set Strategy Casho
+        
