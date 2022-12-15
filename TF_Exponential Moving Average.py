be06731529983ede0733817b262e87ccc7cb4189
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-15 23:23:19 +0300

    Update TF_Exponential Moving Average.py

--- TF_Exponential Moving Average.py
+++ TF_Exponential Moving Average.py
@@ -7,3 +7,12 @@ pair    = 0
 
 # Mass Imports 
 my_data = mass_import(pair, horizon)
+
+# Indicator Parameters
+lookback = 100
+
+def ma(Data, lookback, close, where): 
+    
+    Data = adder(Data, 1)
+    
+    for i in range(len(Data)):
