1a29ed522fbea6a87f645f8aad98e6f6e3eaa6dd
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-16 23:15:27 +0300

    Update NO EXIT CAN STILL BE AN EXIT.py

--- NO EXIT CAN STILL BE AN EXIT.py
+++ NO EXIT CAN STILL BE AN EXIT.py
@@ -7,3 +7,13 @@ class UpgradedRedOrangeGorilla(QCAlgorithm):
         self.SetStartDate(2015, 12, 1)  # Set Start Date
         self.SetEndDate(2020, 12, 1)
         self.SetCash(100000)  # Set Strategy Cash
+        #EURUSD", "USDJPY", "GBPUSD", "AUDUSD" "USDCAD",
+        #"GBPJPY", "EURUSD", "AUDUSD", "EURJPY", "EURGBP"
+        
+        self.Data = {}
+
+        for ticker in ["EURUSD","NZDUSD","USDJPY"]:
+            symbol = self.AddForex(ticker, Resolution.Hour, Market.FXCM).Symbol
+            self.Data[symbol] = SymbolData(self, symbol)
+            
+        
