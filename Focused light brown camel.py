fe4ba5e28a886ce092799a86ac658380d8ffb8e8
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-18 23:58:11 +0300

    Update Focused light brown camel.py

--- Focused light brown camel.py
+++ Focused light brown camel.py
@@ -2,3 +2,14 @@
 from AlgorithmImports import *
 #endregion
 from datetime import datetime, timedelta
+
+
+class FocusedLightBrownCamel(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 7)
+        self.SetEndDate(2020, 1, 7)
+        self.SetCash(100000)  # Set Strategy Cash
+        # self.AddEquity("SPY", Resolution.Minute)
+        
+
