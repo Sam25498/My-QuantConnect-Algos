732d7c825b75d45a6bb8f5aa3cf83ad7d2cced5f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-18 23:58:37 +0300

    Update Focused light brown camel.py

--- Focused light brown camel.py
+++ Focused light brown camel.py
@@ -12,4 +12,18 @@ class FocusedLightBrownCamel(QCAlgorithm):
         self.SetCash(100000)  # Set Strategy Cash
         # self.AddEquity("SPY", Resolution.Minute)
         
+        tickers = ["AAPL","AMZN","TSLA","FB"]
+        #tickers = ["EURUSD","GBPUSD","AUDUSD","USDJPY"]
+        for ticker in tickers:
+            self.AddSecurity(SecurityType.Equity, ticker, Resolution.Daily)
+            #self.symbols = [Symbol.Create(ticker, SecurityType.Equity, Market.USA) for ticker in tickers]
+            self.fastsma = self.SMA(ticker, 30, Resolution.Daily) 
+            self.slowsma = self.SMA(ticker, 90, Resolution.Daily)
+            
+        #self.AddAlpha(CustomSimpleMovingAverage())
+            
+        #self.SetRiskManagement(MaximumDrawdownPercentPerSecurity(0.20))
+        #self.SetUniverseSelection(ManualUniverseSelectionModel(self.symbols))
+        #self.SetExecution(ImmediateExecutionModel())
+
 
