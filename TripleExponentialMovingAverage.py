d92e40b5d0a87d42a29ce5c422085bbd9964f9da
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-20 23:52:56 +0300

    Update TripleExponentialMovingAverage.py

--- TripleExponentialMovingAverage.py
+++ TripleExponentialMovingAverage.py
@@ -6,3 +6,5 @@ self.symbol = self.AddEquity("SPY").Symbol
 # In OnData()
 if data.ContainsKey(self.symbol):
     self.tema.Update(data[self.symbol].EndTime, data[self.symbol].High)
+if self.tema.IsReady:
+    
