3271ee1d1dce6050ccb6a4084640e2e4d74ffca3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-22 23:54:38 +0300

    Create The EMA Trading Strategy.py

---
+++ The EMA Trading Strategy.py
@@ -0,0 +1,11 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+FastisOverSlow = None
+SlowisOverFast = None
+AboveLong = None
+BelowLong = None
+
+
+    
