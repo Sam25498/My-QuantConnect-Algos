3a08bc0afbdca0861e5cbcb7e9e69c7aec623eb8
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-22 23:55:07 +0300

    Update The EMA Trading Strategy.py

--- The EMA Trading Strategy.py
+++ The EMA Trading Strategy.py
@@ -7,5 +7,17 @@ SlowisOverFast = None
 AboveLong = None
 BelowLong = None
 
+class DancingRedBull(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2015, 1, 30)  # Set Start Date
+        self.SetEndDate(2020, 1, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "EURUSD" #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.Data = {}
+
+  
 
     
