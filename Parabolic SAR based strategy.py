22e29dbb1fbcb8a04b41fb919f7a41b40cf901b8
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-24 15:35:18 +0300

    Update Parabolic SAR based strategy.py

--- Parabolic SAR based strategy.py
+++ Parabolic SAR based strategy.py
@@ -4,3 +4,10 @@ from quantconnect.algorithm import *
 
 class ParabolicSARAlgorithm(QCAlgorithm):
 
+    def Initialize(self):
+        # Set the account size and leverage
+        self.SetCash(10000)
+        self.SetBrokerageModel(BrokerageName.OandaBrokerage)
+        self.SetLeverage(50)
+
+        
