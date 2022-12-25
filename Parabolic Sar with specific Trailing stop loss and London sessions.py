a5d17cdb26277d205c78692c8bafec4e8f620e45
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-25 14:19:38 +0300

    Update Parabolic Sar with specific Trailing stop loss and London sessions.py

--- Parabolic Sar with specific Trailing stop loss and London sessions.py
+++ Parabolic Sar with specific Trailing stop loss and London sessions.py
@@ -23,4 +23,26 @@ class ParabolicSARAlgorithm(QCAlgorithm):
         self.stop_loss = -5
 
     def OnData(self, data):
+        
+        #Get the current price and time
+        price = self.Securities[self.symbol].Price
+        time = self.Time
+
+        # Check if we are in the London session (8am-5pm GMT)
+        if time.hour >= 8 and time.hour < 17:
+            # Check if we have a position in the market
+            if self.Portfolio[self.symbol].Invested:
+                # Check if we need to exit the position
+                if self.psar.Current.Value > price:
+                    self.Sell(self.symbol, self.order_size)
+            else:
+                # Check if we need to enter the market
+                if self.psar.Current.Value < price:
+                    self.Buy(self.symbol, self.order_size)
+                    self.SetTrailingStop(self.symbol, self.stop_loss)
+        else:
+            # Close any open positions outside of the London session
+            if self.Portfolio[self.symbol].Invested:
+                self.Liquidate(self.symbol)
+
     
