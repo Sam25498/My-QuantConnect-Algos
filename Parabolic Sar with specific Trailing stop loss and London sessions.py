fa6ad644bf8dfebf59bed7073d26761c7e915849
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-25 14:07:38 +0300

    Create Parabolic Sar with specific Trailing stop loss and London sessions.py

---
+++ Parabolic Sar with specific Trailing stop loss and London sessions.py
@@ -0,0 +1,21 @@
+
+from quantconnect.algorithm import *
+
+class ParabolicSARAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        # Set the account size and leverage
+        self.SetCash(10000)
+        self.SetBrokerageModel(BrokerageName.OandaBrokerage)
+        self.SetLeverage(50)
+
+        # Set the symbols and time frame
+        self.symbol = "EURUSD"
+        self.SetStartDate(2020, 1, 1)
+        self.SetEndDate(2020, 12, 31)
+        self.SetTimeZone("Europe/London")
+        self.AddForex(self.symbol, Resolution.Minute, Market.Oanda)
+
+        # Initialize the Parabolic SAR
+        self.psar = self.PSAR(self.symbol, 0.01, 0.01)
+
