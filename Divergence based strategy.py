846535ee5408c250d0ec2581ec540ea6c1965e97
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-29 13:30:35 +0300

    Update Divergence based strategy.py

--- Divergence based strategy.py
+++ Divergence based strategy.py
@@ -22,5 +22,10 @@ rsiwindow = rollingwindow(50)[indicatordatapoint]
 # Asuming we know our hills and valleys
 #What datapoints will we use
 divergence1 = ch1 < ch2 and rv1 > rv2
+divergence2 = cv1 < cv2 and rh1 > rh2
+#is there a scenario where divergence can happen when hills of both the price and
+#rsi diverge or valleys of both price and rsi diverge
 
+#When the divergence is noticed make sure you log the time and date to cross check on an
+#actual chart.
 
