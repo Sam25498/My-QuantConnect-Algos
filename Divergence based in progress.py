cf533770757b8e97fd1fc2c821f9e7773e3549f5
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-30 20:26:33 +0300

    Create Divergence based in progress.py

---
+++ Divergence based in progress.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 29 19:12:49 2022
+
+@author: Sam
+"""
+
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+import numpy as np
+
+Macdlong = None
+AboveSupport = None
+BelowResistance = None
+
+class CreativeYellowTapir(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2020,1, 30)  # Set Start Date
+        self.SetEndDate(2020,12, 30)
+        self.SetCash(10000)  # Set Strategy Cash
+        
+        self.ticker = "USDCAD"
+        # Rolling Windows to hold bar close data keyed by symbol.
+        self.Data = {}
+
+        #for ticker in tickers:
+        symbol = self.AddForex(self.ticker, Resolution.Hour, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+        self.tolerance = 0.0025
+        self.toleranceR = 0.986761994
+        self.toleranceS = 1.004000555
+        self.stopLossLevel = -0.05 # stop loss percentage 
+        self.stopProfitLevel = 0.01# stop profit percentage
+            
+        self.SetWarmUp(400, Resolution.Hour)
+        
+        
