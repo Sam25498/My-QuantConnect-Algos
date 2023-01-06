d40c95756c42c3b339cf7b5fc9ca72d143fd97f9
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-06 23:47:17 +0300

    Update New EMA Triple Cross.py

--- New EMA Triple Cross.py
+++ New EMA Triple Cross.py
@@ -11,7 +11,29 @@ PreviousFastAbovePreviousM = None
 PreviousFastBelowPreviousM = None
 
 class MuscularRedOrangeHorse(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 30)  # Set Start Date
+        self.SetEndDate(2020, 1, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "USDCHF" #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.Data = {}
+
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker , Resolution.Hour, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+    
+        self.tolerance = 1.001
+     
+        self.stopLossLevel = -0.05 # stop loss percentage 
+        self.stopProfitLevel = 0.01# stop profit percentage
+        
+            
+        self.SetWarmUp(50, Resolution.Hour)
+        
 
 
-  
+
     
