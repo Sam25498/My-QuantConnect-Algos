eb1694923ad5b26fc62c89c098c0e212c3e04d99
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-06 23:46:26 +0300

    Create New EMA Triple Cross.py

---
+++ New EMA Triple Cross.py
@@ -0,0 +1,17 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+
+from datetime import datetime,timedelta
+FastisOverSlow = None
+SlowisOverFast = None
+FastisOverMedium = None
+MediumisOverFast = None
+PreviousFastAbovePreviousM = None
+PreviousFastBelowPreviousM = None
+
+class MuscularRedOrangeHorse(QCAlgorithm):
+
+
+  
+    
