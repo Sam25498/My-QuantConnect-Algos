d3dba654d0fae5e690171aa004e606bf12ead5ef
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-08 11:45:53 +0300

    Create EMA Triple Cross 15 min.py

---
+++ EMA Triple Cross 15 min.py
@@ -0,0 +1,80 @@
+
+#region imports
+from AlgorithmImports import *
+#endregion
+
+from datetime import datetime,timedelta
+FastisOverSlow = None
+SlowisOverFast = None
+FastisOverMedium = None
+MediumisOverFast = None
+PreviousFastAbovePreviousM = None
+PreviousFastBelowPreviousM = None
+
+class MuscularRedOrangeHorse(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 30)  # Set Start Date
+        self.SetEndDate(2020, 1, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "EURGBP" #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.Data = {}
+
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker , Resolution.Minute, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+    
+        self.tolerance = 1.001
+     
+        self.stopLossLevel = -0.0020 # stop loss percentage 
+        self.stopProfitLevel = 0.0020# stop profit percentage
+        
+            
+        self.SetWarmUp(50, Resolution.Minute)
+        
+
+
+    def OnData(self, data):
+        
+        if self.IsWarmingUp: #Data to warm up the algo is being collected.
+            return
+        
+        for symbol, symbolData in self.Data.items(): #Return the dictionary's key-value pairs:
+            if not (data.ContainsKey(symbol) and data[symbol] is not None and symbolData.IsReady):
+                continue
+            
+            slowEMA = symbolData.slowema.Current.Value
+            fastEMA = symbolData.fastema.Current.Value
+            mediumEMA = symbolData.mediumema.Current.Value
+            previousf = symbolData.fastWindow[1]
+            previousm = symbolData.mediumWindow[1]
+            
+            #current_price = data[symbol].Close
+            current_price = symbolData.closeWindow[0]
+            
+            
+
+            if self.Portfolio[symbol].Invested:
+                
+                if self.isLong:
+                    condStopProfit = (current_price - self.buyInPrice) > self.stopProfitLevel
+                    condStopLoss = (current_price - self.buyInPrice) < self.stopLossLevel
+                    if condStopProfit:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Long Position Stop Profit at {current_price}")
+                        
+                    if condStopLoss:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Long Position Stop Loss at {current_price}")
+                else:
+                    condStopProfit = (self.sellInPrice - current_price)/self.sellInPrice > self.stopProfitLevel
+                    condStopLoss = (self.sellInPrice - current_price)/self.sellInPrice < self.stopLossLevel
+                    if condStopProfit:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Short Position Stop Profit at {current_price}")
+                        
+
+    
