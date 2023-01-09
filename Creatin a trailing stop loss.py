5c08a11407a34bf41fbdcdb73476993bf2d000c2
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-09 18:03:17 +0300

    Update Creatin a trailing stop loss.py

--- Creatin a trailing stop loss.py
+++ Creatin a trailing stop loss.py
@@ -31,20 +31,23 @@ if self.Portfolio[symbol].Invested:
                                                 
                         
                     
-                else:
-                    condStopProfit = (profit / cash) > self.stopProfitLevel
-                    condStopLoss = (profit / cash) < self.stopLossLevel
-                    if condStopProfit:
-                        self.Liquidate(symbol)
-                        self.Log(f"{self.Time} Short Position Stop Profit at {current_price}")
-                        
-                    if condStopLoss:
-                        self.Liquidate(symbol)
-                        self.Log(f"{self.Time} Short Position Stop Loss at {current_price}")
-            
-            profit = self.Portfolio[symbol].UnrealizedProfit
-            #profit = self.Portfolio[symbol].NetProfit #Gets the unrealized profit as a percentage of holdings cost
-            cash = self.Portfolio.Cash #Sum of all currencies in account in US dollars (only unsettled cash)
+            else:
+                        condStopLoss = (profit / cash) < self.stopLossLevel
+                        #condTrailingStop = (profit / cash) < (self.stopLossLevel + self.trailingSL)
+                        # if 0.06 > 0.05:
+                        if (profit / cash) > self.trailingSL:
+                                    self.stopLossLevel = self.stopLossLevel + self.trailingSL
+                                    condTrailingStop = (profit / cash) < self.stopLossLevel
+                                    self.trailingSL += 0.05
+                                    
+                        if condStopLoss:
+                                    self.Liquidate(symbol)
+                                    self.Log(f"{self.Time} Long Position Stop Loss at {current_price}")
+                                    
+                        if condTrailingStop:
+                                    self.Liquidate(symbol)
+                                    self.Log(f"{self.Time} Long Position Trailing Stop Loss at {current_price}")  
+                   
 
             if self.Portfolio[symbol].Invested:
                 
