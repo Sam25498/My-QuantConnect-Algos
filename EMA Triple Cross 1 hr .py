10dcaa70bee87dc13eedb86cfd58cc514ae98008
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-09 12:10:52 +0300

    Update EMA Triple Cross 1 hr .py

--- EMA Triple Cross 1 hr .py
+++ EMA Triple Cross 1 hr .py
@@ -84,7 +84,30 @@ class MuscularRedOrangeHorse(QCAlgorithm):
                     if condStopLoss:
                         self.Liquidate(symbol)
                         self.Log(f"{self.Time} Short Position Stop Loss at {current_price}")
-            
+                        
+            if not self.Portfolio[symbol].Invested:
+                FastisOverSlow = fastEMA > slowEMA #* self.tolerance
+                SlowisOverFast = slowEMA > fastEMA #* self.tolerance
+                FastisOverMedium = fastEMA > mediumEMA #* self.tolerance
+                MediumisOverFast = mediumEMA > fastEMA #* self.tolerance
+                PreviousFastBelowPreviousM = previousf < previousm 
+                PreviousFastAbovePreviousM = previousf > previousm
+                
+                
+                if FastisOverSlow and FastisOverMedium and PreviousFastBelowPreviousM and self.LondonSession: #
+                    self.SetHoldings(symbol, 1)
+                    # get buy-in price for trailing stop loss/profit
+                    self.buyInPrice = current_price
+                    # entered long position
+                    self.isLong = True
+                    self.Log(f"{self.Time} Entered Long Position at {current_price}")
+                        
+                if SlowisOverFast and MediumisOverFast  and PreviousFastAbovePreviousM and self.LondonSession: #
+                    self.SetHoldings(symbol, -1)
+                    # get sell-in price for trailing stop loss/profit
+                    self.sellInPrice = current_price
+                    # entered short position
+         
             
             
 
