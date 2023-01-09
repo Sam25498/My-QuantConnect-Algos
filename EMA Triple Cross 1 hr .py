5a2307a9a2c685cb814e8dba5a142b5736d1986a
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-09 12:08:25 +0300

    Create EMA Triple Cross 1 hr .py

---
+++ EMA Triple Cross 1 hr .py
@@ -0,0 +1,30 @@
+
+#region imports
+from AlgorithmImports import *
+#endregion
+
+from datetime import datetime,timedelta
+FastisOverSlow = None
+SlowisOverFast = None
+FastisOverMedium = None
+MediumisOverFast = None
+PreviousFastAbovePreviousM = None
+PreviousFastBelowPreviousM = None
+
+class MuscularRedOrangeHorse(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)  # Set Start Date
+        self.SetEndDate(2020, 12, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "EURUSD"  #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.Data = {}
+
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker , Resolution.Hour, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+    
+
