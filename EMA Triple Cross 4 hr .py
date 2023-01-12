03726116e1a25023d0ded9eabfdf0233c030f232
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-12 18:14:03 +0300

    Update EMA Triple Cross 4 hr .py

--- EMA Triple Cross 4 hr .py
+++ EMA Triple Cross 4 hr .py
@@ -21,6 +21,35 @@ class MuscularRedOrangeHorse(QCAlgorithm):
         self.ticker = "EURUSD"  #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
         # Rolling Windows to hold bar close data keyed by symbol
         self.Data = {}
+        
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker , Resolution.Hour, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+    
+        self.tolerance = 1.001
+     
+        self.stopLossLevel = -0.005 #-0.01#-0.01# stop loss percentage 
+        self.stopProfitLevel = 0.01 #0.02 #0.01#stop profit percentage
+        
+            
+        self.SetWarmUp(50, Resolution.Hour)
+        
+
+
+    def OnData(self, data):
+        
+        if self.IsWarmingUp: #Data to warm up the algo is being collected.
+            return
+        
+        #self.LondonSession = self.Time.hour > 6 and self.Time.hour < 14
+
+        for symbol, symbolData in self.Data.items(): #Return the dictionary's key-value pairs:
+            if not (data.ContainsKey(symbol) and data[symbol] is not None and symbolData.IsReady):
+                continue
+            
+
+
 
 
     
