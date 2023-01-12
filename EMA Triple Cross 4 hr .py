cb9116dfcff7f28b2df5a2c9eaa774abc6b33ce4
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-12 18:13:19 +0300

    Update EMA Triple Cross 4 hr .py

--- EMA Triple Cross 4 hr .py
+++ EMA Triple Cross 4 hr .py
@@ -11,7 +11,17 @@ MediumisOverFast = None
 PreviousFastAbovePreviousM = None
 PreviousFastBelowPreviousM = None
 
+class MuscularRedOrangeHorse(QCAlgorithm):
 
-    
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)  # Set Start Date
+        self.SetEndDate(2020, 12, 30)
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        self.ticker = "EURUSD"  #"USDJPY","GBPUSD",  "USDCAD","EURUSD".
+        # Rolling Windows to hold bar close data keyed by symbol
+        self.Data = {}
+
+
     
     
