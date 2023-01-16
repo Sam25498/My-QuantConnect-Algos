74640622980625f65adaba31541760f4f1cb8de3
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-16 17:14:18 +0300

    Update New Hourly Support and Resistance algo.py

--- New Hourly Support and Resistance algo.py
+++ New Hourly Support and Resistance algo.py
@@ -86,8 +86,6 @@ class SwimmingFluorescentPinkShark(QCAlgorithm):
             if self.Portfolio[symbol].Invested:
                 
                 if self.isLong:
-                    
-     return self.macd.IsReady and self.rsi.IsReady and self.lowWindow.IsReady and self.highWindow.IsReady  
                     condStopProfit = (current_price - self.buyInPrice)/self.buyInPrice > self.stopProfitLevel
                     condStopLoss = (current_price - self.buyInPrice)/self.buyInPrice < self.stopLossLevel
                     if condStopProfit:
