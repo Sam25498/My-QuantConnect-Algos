4e6bee112dc8ee2f7ea4e048fd83acda1e9acdab
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-18 16:36:24 +0300

    Update Hourly Support and Resistance Condition 1 2 and 3.py

--- Hourly Support and Resistance Condition 1 2 and 3.py
+++ Hourly Support and Resistance Condition 1 2 and 3.py
@@ -163,4 +163,32 @@ class SwimmingFluorescentPinkShark(QCAlgorithm):
                     
                     
 
-           
+    def NextSupport(self, window, variation = 0.005, h = 3): 
+        
+        series = window
+        supports = []
+       
+        minima = []
+        
+        # finding maxima and minima by looking for hills/troughs locally..........
+        for i in range(h, series.Size-h):
+            if series[i] < series[i-1] and series[i] < series[i+1] and series[i+1] < series[i+2] and series[i-1] < series[i-2]:
+                minima.append(series[i])
+        
+        # identify minima which are supports
+        for l in minima:
+            r = l * variation
+            # minima which are near each other
+            commonLevel = [x for x in minima if x > l - r and x < l + r]
+            # if 2 or more minima are clustered near an area, it is a support.
+            if len(commonLevel) > 1:
+                # We pick the lowest minima of the cluster as our support
+                level = min(commonLevel)
+                if level not in supports:
+                    supports.append(level)
+                    
+        return supports
+                        
+                    
+
+        return self.lowWindow.IsReady and self.highWindow.IsReady and self.engulfing.IsReady           
