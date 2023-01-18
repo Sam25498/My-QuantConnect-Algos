7beb2b6ab7be010936421a1567a9015096eba318
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-18 16:45:22 +0300

    Create Hourly Support and Resistance Condition 1 2 and 3 Updated.py

---
+++ Hourly Support and Resistance Condition 1 2 and 3 Updated.py
@@ -0,0 +1,39 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import datetime,timedelta
+import numpy as np
+from QuantConnect.Indicators.CandlestickPatterns import Engulfing
+
+
+BelowSupport = None
+AboveResistance = None
+
+
+class SwimmingFluorescentPinkShark(QCAlgorithm):
+
+
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)  # Set Start Date
+        self.SetEndDate(2020, 12, 30)
+        self.SetCash(10000)  # Set Strategy Cash
+        
+        self.ticker = "EURUSD"
+        # Rolling Windows to hold bar close data keyed by symbol.
+        self.Data = {}
+
+        self.engulfing = Engulfing()
+        #for self.ticker in self.tickers:
+        symbol = self.AddForex(self.ticker, Resolution.Hour, Market.Oanda).Symbol
+        self.Data[symbol] = SymbolData(self, symbol)
+         
+        self.tolerance = 0.0025
+        self.toleranceR = 0.986761994
+        self.toleranceS = 1.004000555
+        self.stopLossLevel = -0.05 # stop loss percentage 
+        self.stopProfitLevel = 0.01# stop profit percentage
+            
+        self.SetWarmUp(200, Resolution.Hour)
+        
+        
+
