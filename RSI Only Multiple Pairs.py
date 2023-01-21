61e3c9da9f2738cc2e6e8e6a4264f9835344a2dc
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-21 19:26:18 +0300

    Update RSI Only Multiple Pairs.py

--- RSI Only Multiple Pairs.py
+++ RSI Only Multiple Pairs.py
@@ -38,4 +38,53 @@ class FocusedYellowLemur(QCAlgorithm):
 
 
 
+            if self.Portfolio[symbol].Invested:
+                
+                if self.isLong:
+                    condStopProfit = (current_price - self.buyInPrice)/self.buyInPrice > self.stopProfitLevel
+                    condStopLoss = (current_price - self.buyInPrice)/self.buyInPrice < self.stopLossLevel
+                    if condStopProfit:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Long Position Stop Profit at {current_price}")
+                        
+
+                    if condStopLoss:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Long Position Stop Loss at {current_price}")
+                  
+                else:
+                    condStopProfit = (self.sellInPrice - current_price)/self.sellInPrice > self.stopProfitLevel
+                    condStopLoss = (self.sellInPrice - current_price)/self.sellInPrice < self.stopLossLevel
+                    if condStopProfit:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Short Position Stop Profit at {current_price}")
+                        
+                    if condStopLoss:
+                        self.Liquidate(symbol)
+                        self.Log(f"{self.Time} Short Position Stop Profit at {current_price}")
+                        
+    
             
+            if not self.Portfolio[symbol].Invested:
+                
+                
+                if rsi < 15:
+                    self.SetHoldings(symbol, 1)
+                    # get buy-in price for trailing stop loss/profit
+                    self.buyInPrice = current_price
+                    # entered long position
+                    self.isLong = True
+                    #Timebought = self.Time
+                    self.Log(f"{self.Time} Entered Long Position at {current_price}")
+     
+                        
+                if rsi > 60:
+                    self.SetHoldings(symbol, -1)
+                    # get sell-in price for trailing stop loss/profit
+                    self.sellInPrice = current_price
+                    # entered short position
+                    self.isLong = False
+                    #Timesold = self.Time
+                    self.Log(f"{self.Time} Entered Short Position at {current_price}")
+                        
+                    
