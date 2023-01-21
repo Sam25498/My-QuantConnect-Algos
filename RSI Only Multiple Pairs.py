708694086b469086ae5d4111b8bee5174ec5964c
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-21 19:25:15 +0300

    Create RSI Only Multiple Pairs.py

---
+++ RSI Only Multiple Pairs.py
@@ -0,0 +1,26 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+from datetime import timedelta, datetime
+class FocusedYellowLemur(QCAlgorithm):
+
+
+    def Initialize(self):
+        self.SetStartDate(2015, 12, 1)  # Set Start Date
+        self.SetEndDate(2016, 12, 1)
+        self.SetCash(100000)  # Set Strategy Cash
+       
+        self.Data = {}
+
+        for ticker in ["EURUSD","USDJPY", "USDCHF", "AUDUSD"]:
+            symbol = self.AddForex(ticker, Resolution.Hour, Market.Oanda).Symbol
+            self.Data[symbol] = SymbolData(self, symbol)
+            
+            
+            
+        self.stopLossLevel = -0.05 # stop loss percentage 
+        self.stopProfitLevel = 0.01# stop profit percentage
+            
+        self.SetWarmUp(100, Resolution.Hour)
+
+
