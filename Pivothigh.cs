7e2e06324089d385252f930481912dbd99164a2e
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-28 22:21:27 +0300

    Create Pivothigh.cs

---
+++ Pivothigh.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using BitMexLibrary;
+
+namespace MachinaTrader.Indicators
+{
+
+	public static partial class Extensions
+	{
+		public static List<decimal?> PivotHigh(this List<Candle> source, int barsLeft = 4, int barsRight = 2, bool fillNullValues = false)
+		{
+			var result = new List<decimal?>();
+            
+			for (int i = 0; i < source.Count; i++)
+			{
+				if (i < barsLeft + barsRight)
+				{
+					result.Add(null);
+					continue;
+				}
+                
+				var isPivot = true;
+				var subSet = source.Skip(i - barsLeft - barsRight).Take(barsLeft + barsRight + 1).ToList();
+				var valueToCheck = subSet[barsLeft];
+
