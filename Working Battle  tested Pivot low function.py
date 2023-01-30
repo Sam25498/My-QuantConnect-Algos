1841acf975f98559472cf9c27f12c8da366f7813
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-30 16:17:17 +0300

    Update Working Battle  tested Pivot low function.py

--- Working Battle  tested Pivot low function.py
+++ Working Battle  tested Pivot low function.py
@@ -31,3 +31,34 @@ def pivot_low(source, bars_left: int = 4, bars_right: int = 2, fill_null_values:
         return fill_pivot_nulls(result)
     return result
 
+def fill_pivot_nulls(result: List[Optional[float]]) -> List[Optional[float]]:
+    values = []
+    null_counter = 0
+    for item in result:
+        if item is not None:
+            values.append((item, null_counter))
+            null_counter = 0
+        else:
+            null_counter += 1
+    final_list = []
+    is_first = True
+    for i in range(len(values)):
+        if is_first:
+            for j in range(values[i][1]):
+                final_list.append(None)
+            final_list.append(values[i][0])
+            is_first = False
+        else:
+            current = values[i]
+            previous = values[i - 1]
+            count = current[1]
+            for x in range(1, count + 1):
+                if current[0] > previous[0]:
+                    amount_to_use = (current[0] - previous[0]) / (count + 1)
+                    final_list.append(round(previous[0] + (amount_to_use * x), 8))
+                else:
+                    final_list.append(previous[0])
+    return final_list
+    
+    
+print(pivot_low(Candle))
